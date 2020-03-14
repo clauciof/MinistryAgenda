dedef0023bc02e294a5ddb1a254a3682ba22f003
clauciof <claucio2@gmail.com>
2020-03-13 21:48:42 -0300

    Scrapy da página inicial realizado

---
+++ ministry_agenda/ministry_agenda/spiders/spider.py
@@ -0,0 +1,79 @@
+from bs4 import BeautifulSoup
+import scrapy
+import pandas as pd
+
+
+class MinistrySpider(scrapy.Spider):
+    name = 'Ministryspider'
+    start_urls = ['http://www.economia.gov.br/agendas/gabinete-do-ministro/ministro-da-economia/paulo-guedes/2020-03-13/']
+    MinistryName = 'Ministério da Economia'
+    MinisterName = 'Paulo Guedes'
+    
+    
+    
+    def parse(self, response):
+        
+        soup = BeautifulSoup(response.body, 'lxml')
+        
+        #extraindo os dados
+        events = response.xpath('//div[@class="comprimisso-dados"]').getall()
+        event_title = [x.string for x in soup.select('.comprimisso-titulo')] #Sim, a classe se chama comprimisso
+        event_date = response.xpath('//time[@class="horario comprimisso-inicio"]/@datetime').getall()
+        event_location = [x.text for x in soup.select('.comprimisso-local')]
+        event_participants = response.xpath('//p[@class="comprimisso-participantes"]').getall()
+        
+
+
+        d_participants = self.trata_participantes(response, event_participants)
+        
+        self.salva_csv(event_title, event_date, event_location, event_participants, d_participants)
+    
+
+
+
+
+
+
+
+
+    def salva_csv(self, et, ed, el, ep, dp):
+        df = pd.DataFrame(columns = None)
+        for i in range(0, len(et)):
+            dicionario = {'MinistryName': 'Ministério da Economia', 'MinisterName': 'Paulo Guedes', 
+            'EventDate': ed[i] , 'EventTitle': et[i], 'EventLocation': el[i].replace('\nLocal:', ' '), 'EventParticipants': [dp[i]]}
+            _df = pd.DataFrame(dicionario, index=[0])
+            df = df.append(_df, ignore_index=True)
+
+        with open('data.csv', 'a', encoding="utf-8") as f:
+            df.to_csv(f, header=f.tell()==0, index=False)
+       
+    
+
+
+
+
+
+
+    def trata_participantes(self, response, ep):
+        events = response.xpath('//div[@class="comprimisso-dados"]').getall()
+
+        dicionario = {}
+        for i in range(0, len(events)):
+            if events[i].find("comprimisso-participantes") == -1:
+                dicionario[i] = "N/A"
+            else:
+                dicionario[i] = events[i].find("comprimisso-participantes")
+
+        y=0
+        for i in range(0,len(events)):
+            if dicionario[i] != "N/A":
+                string = ep[y]
+                dicionario[i] = string[56:] # 56 = len(<p class="comprimisso-participantes ...")
+                dicionario[i] = dicionario[i].replace('\r<br>', ', ')
+                dicionario[i] = dicionario[i].replace('</p>', '')
+                dicionario[i] = dicionario[i].split(', ')
+                y = y+1
+
+        return dicionario
+        
+    
